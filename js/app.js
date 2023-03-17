256d81644c3af73cefb717625881a0427e6a376b
cameron <hammerbro_97@hotmail.com>
2023-03-16 20:02:28 -0600

    stuff

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let usernames = [`Alex`, `Cat`, `Dog`, `Fish`, `Eats`];
+let user_ages = [20, 19, 31, 49, 25];
+
+user_ages.push(70);
+usernames.push('Fox');
+
+
+
+let lastUsername = usernames.pop();
+let lastUserAge = user_ages.pop();
+let first_user_name = usernames[0];
+let last_user_name = usernames[4];
+let middle_user_name = usernames[2];
